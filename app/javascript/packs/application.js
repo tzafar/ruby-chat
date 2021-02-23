1825551b1acd953861d7afb3f78bc52db82ff876
Toseef Zafar <mtouseef.zafar@gmail.com>
2021-02-23 21:50:01 +0000

    added semantic UI and top level sample menu

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,3 +11,9 @@ import "channels"
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+require("semantic-ui-sass")
+
+$(document).on("turbolinks:load", function(){
+    $(".ui.dropdown").dropdown()
+});
+
