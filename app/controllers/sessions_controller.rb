e080cf34c24531d6b7a4ac8cd3b1ae771e7a7979
Toseef Zafar <mtouseef.zafar@gmail.com>
2021-02-26 09:06:19 +0000

    Authentication

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,23 @@
 class SessionsController < ApplicationController
   def new
+    @user = User.new
+  end
 
+  def create
+    user = User.find_by_username(params[:session][:username])
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      flash[:success] = 'Logged in successfully'
+      redirect_to root_path
+    else
+      flash.now[:error] = 'Credentials not correct'
+      render :new
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    flash[:success] = 'Successfully Logged out'
+    redirect_to login_path
   end
 end
