555a58272f9ff17b8e50e4652fa3f564bb6e4942
Toseef Zafar <mtouseef.zafar@gmail.com>
2021-03-04 14:13:52 +0000

    turn of csrf

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,6 @@ module MessageMe
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-
+    config.action_controller.allow_forgery_protection = false
   end
 end
