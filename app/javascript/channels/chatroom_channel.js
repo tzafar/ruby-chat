80584d6836f73e77df7bfc0a07f99f4d77c6ea2f
Toseef Zafar <mtouseef.zafar@gmail.com>
2021-03-04 18:22:41 +0000

    textbox should be empty when a message is sent

--- app/javascript/channels/chatroom_channel.js
+++ app/javascript/channels/chatroom_channel.js
@@ -13,6 +13,7 @@ consumer.subscriptions.create("ChatroomChannel", {
     $('#message-container').append (data.mod_message);
     if($('#messages').length > 0){
       $('#messages').scrollTop($('#messages')[0].scrollHeight);
+      $('#message_body').text('')
     }
   }
 });
